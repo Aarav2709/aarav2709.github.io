c375b789677f442c83cb8e70c74225f876baf702
Aarav Gupta <103264776+Aarav2709@users.noreply.github.com>
2026-04-10 17:29:18 +0530

    Final fixes.

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,13 +1,1 @@
 /// <reference types="astro/client" />
-
-interface ImportMetaEnv {
-  readonly PUBLIC_EMAILJS_SERVICE_ID?: string;
-  readonly PUBLIC_EMAILJS_PUBLIC_KEY?: string;
-  readonly PUBLIC_EMAILJS_OWNER_TEMPLATE_ID?: string;
-  readonly PUBLIC_EMAILJS_USER_TEMPLATE_ID?: string;
-  readonly PUBLIC_EMAILJS_TO_EMAIL?: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
